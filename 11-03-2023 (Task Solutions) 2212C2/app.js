c79237915144671054c9b753e9ade16edbb1309d
Sameer Faisal <45840879+SameerFaisal@users.noreply.github.com>
2023-03-11 17:16:54 +0500

    11-03-2023

---
+++ 11-03-2023 (Task Solutions) 2212C2/app.js
@@ -0,0 +1,86 @@
+//Write a JavaScript program to check from two given integers (user input) whether one of them is 8 
+// or their sum or difference or product or quotient is 8.
+
+// let num1=+prompt("Enter number 1")
+// let num2=+prompt("Enter number 2")
+
+// let sum=num1+num2
+// let difference=num1-num2
+// let product=num1*num2
+// let quotient=num1/num2
+
+// if(num1===8 || num2===8){
+//     alert("They are 8")
+// }
+// else if(sum===8){
+//     alert("The sum is 8")
+// }
+// else if(difference===8){
+//     alert("The difference is 8")
+// }
+// else if(product===8){
+//     alert("The product is 8")
+// }
+// else if(quotient===8){
+//     alert("The quotient is 8")
+// }
+
+//Write a JavaScript program to compute the sum of the two given integers. 
+//If the two values are same, then prints triple their sum otherwise just print the sum.
+
+// let num1=+prompt("Enter number 1")
+// let num2=+prompt("Enter number 2")
+// let sum=num1+num2
+// if(num1===num2){
+//     document.write((num1+num2)*3)
+// }
+// else{
+//     document.write(sum)
+// }
+
+// Write a JavaScript program to check three user input numbers, 
+// if the three numbers are same print "All are same" otherwise print "All are different" and if two numbers are same print "Two are same".
+
+// let num1=+prompt("Enter number 1")
+// let num2=+prompt("Enter number 2")
+// let num3=+prompt("Enter number 3")
+// if(num1===num2 && num1===num3){
+//     document.write("All are same")
+// }
+// else if(num1===num2 ||num1===num3||num2===num3){
+//     document.write("Two are same")
+// }
+// else{
+//     document.write("All are different")
+// }
+
+//Write a JavaScript program which iterates the integers from 1 to 100. But for multiples of three print "Fizz" instead of the number 
+// and for the multiples of five print "Buzz". 
+// For numbers which are multiples of both three and five print "FizzBuzz".
+
+// for(let i = 1; i <= 100; i++){
+//     if(i%3===0 && i%5===0){
+//         document.write("FizzBuzz<br>")
+//     }
+//     else if(i%3===0){
+//         document.write("Fizz<br>")
+//     }
+//     else if(i%5===0){
+//         document.write("Buzz<br>")
+//     }
+   
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Arrays/index.js
@@ -0,0 +1,29 @@
+// var numbers=[]
+
+// var howMany=+prompt("How Many values do you want")
+// for(var i=0;i<howMany;i++){
+//    numbers[i]=+prompt("Enter "+i+" Value")
+   
+// }
+
+
+// for(var i=0;i<numbers.length;i++){
+//     document.write(numbers[i]+"<br>")
+// }
+
+
+var names=[]
+
+var howMany=+prompt("How Many values do you want")
+for(var i=0;i<howMany;i++){
+   names[i]=prompt("Enter "+i+" Value")
+   
+}
+
+
+for(var i=0;i<names.length;i++){
+    document.write(names[i]+"<br>")
+}
+
+
+
